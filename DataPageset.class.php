8a1b122d322777c5712b38e045f814fad5ca19df
abuzarhamza <abuzarhamza@gmail.com>
2014-04-04 00:34:10 +0530

    current page function

--- DataPageset.class.php
+++ DataPageset.class.php
@@ -4,7 +4,7 @@
         var $total_entries,$entries_per_page,$current_page,
         $mode,$entries_per_page,$pages_per_set;
 
-        public function __construct($total_entries=0,$entries_per_page=10,$current_page=1,$pages_per_set=10,$mode='slide') {
+        function __construct($total_entries=0,$entries_per_page=10,$current_page=1,$pages_per_set=10,$mode='slide') {
             $this->total_entries    = $total_entries;
             $this->entries_per_page = $entries_per_page;
             $this->current_page     = $current_page;
@@ -12,23 +12,23 @@
             $this->mode             = $mode;
         }
 
-        public function entries_per_page($entries_per_page) {
+        function entries_per_page($entries_per_page) {
             $this->entries_per_page = $entries_per_page
         }
 
-        public function current_page($current_page) {
+        function current_page($current_page) {
             $this->current_page=$current_page;
         }
 
-        public function total_entries($total_entries) {
+        function total_entries($total_entries) {
             $this->total_entries=$total_entries;
         }
 
-        public function total_entries($total_entries) {
+        function total_entries($total_entries) {
             $this->total_entries=$total_entries;
         }
 
-        public function first_page() {
+        function first_page() {
             $this->first_page =1;
             return $this->first_page;
         }
@@ -50,15 +50,18 @@
         //     return $self->_current_page_accessor();
         // }
         //This method gets or sets the current page number
-        public function current_page($current_page=1) {
+        function current_page($current_page=1) {
             // $this->total_entries;
             // $this->entries_per_page;
             //  current_page
-            $this->current_page = $current_page;
-            return $this->current_page;
+            return $this->first_page if (undef() || );
+            return $this->first_page if ($this->current_page < $this->first_page);
+            return $this->last_page if ($this->current_page > $this->last_page);
+
+            return $this->current_page; 
         }
 
-        public function last_page() {
+        function last_page() {
             $pages = $this->total_entries / $this->entries_per_page;
             if ($pages == int($pages) ) {
                 $this->last_page=$pages;
@@ -69,7 +72,7 @@
             return $this->last_page;
         }
 
-        public function enteries_on_this_page() {
+        function enteries_on_this_page() {
             if ($this->total_entries == 0) {
                 return 0;
             } else {
@@ -78,7 +81,7 @@
         }
 
         //This method returns the number of the first entry on the current page:
-        public function first() {
+        function first() {
             if ($this->total_entries == 0 ) {
                 return 0;
             } else {
@@ -87,7 +90,7 @@
         }
 
         //This method returns the number of the last entry on the current page:
-        public function last() {
+        function last() {
             if ( $this->current_page == $this->last_page ) {
                 return $this->total_entries;
             } else {
@@ -95,7 +98,7 @@
             }
         }
 
-        public function previous_page() {
+        function previous_page() {
             if ($this->current_page > 1) {
                 return $self->current_page - 1;
             } else {
