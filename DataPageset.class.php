0bded66ee1442200405f4295c00b667801a076d7
abuzar hamza <abuzarhamza@gmail.com>
2014-04-04 03:15:18 +0530

    many bug fixes

--- DataPageset.class.php
+++ DataPageset.class.php
@@ -2,7 +2,7 @@
     class DataPageset {
 
         var $total_entries,$entries_per_page,$current_page,
-        $mode,$entries_per_page,$pages_per_set;
+        $mode,$pages_per_set;
 
         function __construct($total_entries=0,$entries_per_page=10,$current_page=1,$pages_per_set=10,$mode='slide') {
             $this->total_entries    = $total_entries;
@@ -12,67 +12,55 @@
             $this->mode             = $mode;
         }
 
-        function entries_per_page($entries_per_page) {
-            $this->entries_per_page = $entries_per_page
+        public function entries_per_page($entries_per_page) {
+            $this->entries_per_page = $entries_per_page;
         }
 
-        function current_page($current_page) {
-            $this->current_page=$current_page;
-        }
-
-        function total_entries($total_entries) {
+        public function total_entries($total_entries) {
             $this->total_entries=$total_entries;
         }
 
-        function total_entries($total_entries) {
-            $this->total_entries=$total_entries;
-        }
-
-        function first_page() {
+        public function first_page() {
             $this->first_page =1;
             return $this->first_page;
         }
 
-        // __PACKAGE__->mk_accessors(qw(total_entries entries_per_page current_page));
-        // sub current_page {
-        //     my $self = shift;
-        //     if (@_) {
-        //         return $self->_current_page_accessor(@_);
-        //     }
-        //     return $self->first_page unless defined $self->_current_page_accessor;
-
-        //     return $self->first_page
-        //         if $self->_current_page_accessor < $self->first_page;
-
-        //     return $self->last_page
-        //         if $self->_current_page_accessor > $self->last_page;
-
-        //     return $self->_current_page_accessor();
-        // }
-        //This method gets or sets the current page number
-        function current_page($current_page=1) {
+        public function current_page() {
             // $this->total_entries;
             // $this->entries_per_page;
             //  current_page
-            return $this->first_page if (undef() || );
-            return $this->first_page if ($this->current_page < $this->first_page);
-            return $this->last_page if ($this->current_page > $this->last_page);
+            if (empty($this->total_entries) ||
+                 empty($this->entries_per_page) || empty($this->current_page)
+            ) {
+                return $this->first_page;
+            }
+
+            if ( $this->current_page < $this->first_page ) {
+                return $this->first_page;
+            }
+            if ( $this->current_page > $this->last_page ) {
+                return $this->last_page;
+            }
 
             return $this->current_page; 
         }
 
-        function last_page() {
+        public function last_page() {
+
             $pages = $this->total_entries / $this->entries_per_page;
-            if ($pages == int($pages) ) {
+            if ($pages == intval($pages) ) {
                 $this->last_page=$pages;
             } else {
                 $this->last_page =$pages +1;
             }
-            $this->last_page =1 if ($this->last_page < 1);
+
+            if ( $this->last_page < 1 ) {
+                $this->last_page =1;
+            }
             return $this->last_page;
         }
 
-        function enteries_on_this_page() {
+        public function enteries_on_this_page() {
             if ($this->total_entries == 0) {
                 return 0;
             } else {
@@ -81,7 +69,7 @@
         }
 
         //This method returns the number of the first entry on the current page:
-        function first() {
+        public function first() {
             if ($this->total_entries == 0 ) {
                 return 0;
             } else {
@@ -90,16 +78,16 @@
         }
 
         //This method returns the number of the last entry on the current page:
-        function last() {
-            if ( $this->current_page == $this->last_page ) {
+        public function last() {
+            if ( $this->current_page == $this->last_page() ) {
                 return $this->total_entries;
             } else {
                 return ( $this->current_page * $this->entries_per_page );
             }
         }
 
-        function previous_page() {
-            if ($this->current_page > 1) {
+        public function previous_page() {
+            if ( $this->current_page > 1) {
                 return $self->current_page - 1;
             } else {
                 return undef;
